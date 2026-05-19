a849f37a545c98a8653e366f7389696e332388f7
kaiware0x <kaiware4job@gmail.com>
2026-05-19 10:27:52 +0900

    done 2.4

--- MikanLoaderPkg/Main.c
+++ MikanLoaderPkg/Main.c
@@ -1,6 +1,32 @@
 #include <Library/UefiLib.h>
 #include <Uefi.h>
 
+struct MemoryMap
+{
+    UINTN buffer_size;
+    VOID *buffer;
+    UINTN map_size;
+    UINTN map_key;
+    UINTN descriptor_size;
+    UINT32 descriptor_version;
+};
+
+EFI_STATUS GetMemoryMap(struct MemoryMap *map)
+{
+    if (map->buffer == NULL)
+    {
+        return EFI_BUFFER_TOO_SMALL;
+    }
+
+    map->map_size = map->buffer_size; // 最大書込みサイズを設定しておく
+    return gBS->GetMemoryMap(
+        &map->map_size, // 実際のサイズが格納される
+        (EFI_MEMORY_DESCRIPTOR *)map->buffer,
+        &map->map_key,
+        &map->descriptor_size,
+        &map->descriptor_version);
+}
+
 EFI_STATUS EFIAPI UefiMain(
     EFI_HANDLE image_handle,
     EFI_SYSTEM_TABLE *system_table)
