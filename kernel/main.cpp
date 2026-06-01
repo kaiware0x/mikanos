40f75b237f21a0bb7cdaf42a673cfbe5f593d59b
kaiware0x <kaiware4job@gmail.com>
2026-06-01 15:48:19 +0900

    done 5.1

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -3,6 +3,25 @@
 
 #include "frame_buffer_config.hpp"
 
+const uint8_t kFontA[16] = {
+    0b00000000, //
+    0b00011000, //    **
+    0b00011000, //    **
+    0b00011000, //    **
+    0b00011000, //    **
+    0b00100100, //   *  *
+    0b00100100, //   *  *
+    0b00100100, //   *  *
+    0b00100100, //   *  *
+    0b01111110, //  ******
+    0b01000010, //  *    *
+    0b01000010, //  *    *
+    0b01000010, //  *    *
+    0b11100111, // ***  ***
+    0b00000000, //
+    0b00000000, //
+};
+
 struct PixelColor
 {
     uint8_t r, g, b;
@@ -56,6 +75,23 @@ public:
     }
 };
 
+void WriteAscii(PixelWriter &writer, int x, int y, char c, const PixelColor &color)
+{
+    if (c != 'A')
+        return;
+
+    for (int dy = 0; dy < 16; ++dy)
+    {
+        for (int dx = 0; dx < 8; ++dx)
+        {
+            if ((kFontA[dy] << dx) & 0x80u)
+            {
+                writer.Write(x + dx, y + dy, color);
+            }
+        }
+    }
+}
+
 void *operator new(std::size_t size, void *buf)
 {
     return buf;
@@ -96,6 +132,9 @@ extern "C" void KernelMain(const FrameBufferConfig &frame_buffer_config)
             pixel_writer->Write(100 + x, 100 + y, {0, 255, 0});
         }
     }
+
+    WriteAscii(*pixel_writer, 50, 50, 'A', {0, 0, 0});
+    WriteAscii(*pixel_writer, 66, 50, 'A', {0, 0, 0});
 
     while (1)
         __asm__("hlt");
